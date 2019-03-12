6738ca0950b1d78908d919347445b9743c8c88a8
Willtrigo <willtrigo@gmail.com>
2019-03-11 22:03:55 -0300

    Add leads

---
+++ drwt/leads/models.py
@@ -0,0 +1,11 @@
+"""Leads models settings."""
+from django.db import models
+
+
+class Lead(models.Model):
+    """Lead model."""
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
